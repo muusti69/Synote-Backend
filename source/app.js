2f8d1d0871a0d88531cbf87d3cdab81e7829cf6c
Aryan Singh Thakur <aryana123b456@gmail.com>
2025-06-21 23:11:02 +0530

    Implement cors and cookieParser

--- source/app.js
+++ source/app.js
@@ -1,5 +1,19 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import limitter from "./constant.js";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: limitter }));
+app.use(express.urlencoded({ extended: true, limit: limitter }));
+app.use(cookieParser());
 
 export { app };
--- source/constant.js
+++ source/constant.js
@@ -1,1 +1,2 @@
 export const DB_NAME="AI_Web"
+export const limitter="16kb"
