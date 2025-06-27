2cccb5e1a9198fa281a3736a79cb445a2e2f41ef
Aryan Singh Thakur <aryana123b456@gmail.com>
2025-06-27 23:58:01 +0530

    Implement basic CRUD for subtasks

--- source/controllers/subtask.controller.js
+++ source/controllers/subtask.controller.js
@@ -27,4 +27,72 @@ const createSubtask = asyncHandler(async (req, res) => {
     .json(new apiResponse(201, subtask, "Subtask created successfully"));
 });
 
-export { createSubtask };
+const getSubtasks = asyncHandler(async (req, res) => {
+  const taskId = req.params.id;
+
+  const task = await Task.findOne({ _id: taskId, userId: req.user?._id });
+
+  if (!task) throw new apiError(404, "Task not found or unauthorized");
+
+  const subtasks = await Subtask.find({ taskId }).sort({ updatedAt: -1 });
+
+  return res
+    .status(200)
+    .json(new apiResponse(200, subtasks, "Subtasks for the task fetched"));
+});
+
+const updateSubtask = asyncHandler(async (req, res) => {
+  const taskId = req.params.id;
+  const subtaskId = req.params.subtaskId;
+  const { content } = req.body;
+
+  if (!content?.trim()) {
+    return res
+      .status(400)
+      .json(new apiError(400, "Subtask content is required"));
+  }
+
+  const task = await Task.findOne({ _id: taskId, userId: req.user?._id });
+
+  if (!task) throw new apiError(404, "Task not found or unauthorized");
+
+  const subtask = await Subtask.findOne({ _id: subtaskId, taskId });
+
+  if (!subtask) throw new apiError(404, "Subtask not found");
+
+  if (content?.trim() === subtask.content) {
+    return res
+      .status(200)
+      .json(new apiResponse(200, subtask, "No changes detected"));
+  }
+
+  subtask.content = content.trim();
+  await subtask.save();
+
+  return res
+    .status(200)
+    .json(new apiResponse(200, subtask, "Subtask updated successfully"));
+});
+
+const deleteSubtask = asyncHandler(async (req, res) => {
+  const taskId = req.params.id;
+  const subtaskId = req.params.subtaskId;
+
+  const task = await Task.findOne({ _id: taskId, userId: req.user?._id });
+
+  if (!task) throw new apiError(404, "Task not found or unauthorized");
+
+  const subtask = await Subtask.findOne({ _id: subtaskId, taskId });
+
+  if (!subtask) {
+    throw new apiError(404, "Subtask not found");
+  }
+
+  await subtask.deleteOne();
+
+  return res
+    .status(200)
+    .json(new apiResponse(200, {}, "Subtask deleted successfully"));
+});
+
+export { createSubtask, getSubtasks, updateSubtask,deleteSubtask };
--- source/routes/subtask.routes.js
+++ source/routes/subtask.routes.js
@@ -1,9 +1,17 @@
 import { Router } from "express";
-import { createSubtask } from "../controllers/subtask.controller.js";
+import {
+  createSubtask,
+  getSubtasks,
+  updateSubtask,
+  deleteSubtask,
+} from "../controllers/subtask.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router({ mergeParams: true });
 
 router.route("/").post(verifyJWT, createSubtask);
+router.route("/").get(verifyJWT, getSubtasks);
+router.route("/:subtaskId").patch(verifyJWT, updateSubtask);
+router.route("/:subtaskId").delete(verifyJWT, deleteSubtask);
 
 export default router;
